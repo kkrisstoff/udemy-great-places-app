659afbe163171d17f55d223829a527762f41db09
Kostiantyn Pashura <>
2021-08-04 11:30:38 +0300

    update app config and add SQLite helpre

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -20,25 +20,34 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Consumer<GreatPlaces>(
-          child: Center(
-            child: Text('Got no places yet'),
-          ),
-          builder: (context, greatPlaces, ch) => greatPlaces.items.length <= 0
-              ? ch as Widget
-              : ListView.builder(
-                  itemCount: greatPlaces.items.length,
-                  itemBuilder: (ctx, i) => ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: FileImage(greatPlaces.items[i].image),
-                      ),
-                      title: Text(greatPlaces.items[i].title),
-                      onTap: () {
-                        // go to detail page
-                      }),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: const Text('Got no places yet, start adding some!'),
                 ),
-        ),
+                builder: (context, greatPlaces, ch) =>
+                    greatPlaces.items.length <= 0
+                        ? ch as Widget
+                        : ListView.builder(
+                            itemCount: greatPlaces.items.length,
+                            itemBuilder: (ctx, i) => ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage:
+                                      FileImage(greatPlaces.items[i].image),
+                                ),
+                                title: Text(greatPlaces.items[i].title),
+                                onTap: () {
+                                  // go to detail page
+                                }),
+                          ),
+              ),
       ),
     );
   }
